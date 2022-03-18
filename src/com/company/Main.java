6f6c0f31beda80db4898a0ef41553fb3b57b53a1
Bekzhan <bekjan.jambykulov@gmail.com>
2022-03-18 14:18:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+    
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        System.out.println(set1);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(0);
+        set2.add(1);
+        set2.add(2);
+        System.out.println(set2);
+        symmetricSetDifference(set1, set2);
+
+
+    }
+
+    public static void symmetricSetDifference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> newSet = new HashSet<Integer>(set1);
+        newSet.removeAll(set2);
+        set2.removeAll(set1);
+        newSet.addAll(set2);
+        System.out.println(newSet);
+    }
+}
